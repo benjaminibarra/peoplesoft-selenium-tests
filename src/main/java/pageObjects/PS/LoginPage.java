d8b37a6cc05ef212b7aa7151e52f87d556c97c97
benjoibarra <benjoibarra20@gmail.com>
2020-08-11 20:44:13 +0800

    Set up project

--- src/main/java/pageObjects/PS/LoginPage.java
+++ src/main/java/pageObjects/PS/LoginPage.java
@@ -5,10 +5,16 @@ import net.serenitybdd.core.pages.WebElementFacade;
 import net.thucydides.core.annotations.DefaultUrl;
 import net.thucydides.core.util.SystemEnvironmentVariables;
 import org.openqa.selenium.WebDriver;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import utils.ExplicitWait;
+
+import java.rmi.server.ExportException;
 
 @DefaultUrl("/psp/UFIUAT92/EMPLOYEE/ERP/?&cmd=login&languageCd=ENG")
 public class LoginPage extends GenericPage {
+
+    final Logger LOGGER = LoggerFactory.getLogger(LoginPage.class);
 
     @FindBy(id="userid")
     WebElementFacade userIDInput;
@@ -22,9 +28,19 @@ public class LoginPage extends GenericPage {
     protected WebDriver driver = getDriver();
 
     public void setUserCredentials() {
-        ExplicitWait.explicitlyWaitForPresenceOfElement(getDriver(), 120, userIDInput);
-        typeIntoInput(userIDInput, getUserID(), false);
-        typeIntoInput(passwordInput, getUserPassword(), false);
+        int attempts = 0;
+        while (attempts < 3) {
+            try {
+                ExplicitWait.explicitlyWaitForPresenceOfElement(getDriver(), 40, userIDInput);
+                typeIntoInput(userIDInput, getUserID(), false);
+                typeIntoInput(passwordInput, getUserPassword(), false);
+                break;
+            } catch (Exception e) {
+                LOGGER.error("Login Page did not load properly! Refreshing page...");
+                getDriver().navigate().refresh();
+            }
+            attempts++;
+        }
     }
 
     public void clickSignInBtn () {
